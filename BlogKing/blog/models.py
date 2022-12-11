d6abc82fe7889b72255768b888d6e84ec116c286
Averasi <56447978+Averasi@users.noreply.github.com>
2022-12-10 18:31:57 -0600

    Registration
    [+] Added /register and /login urls
    [+] Added two buttons on the home page for login and register that redirects to their respective urls
    [+] Added basic form for /register allowing the user to enter in a username and password for registration
    [+] Added a view 'Register' in /BlogKing/blog/views.py that handles the data received from the registration form. 
         This view will create a new user if the data provided is successfully validated

--- BlogKing/blog/models.py
+++ BlogKing/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.forms import ModelForm
 
 STATUS = (
     (0,"Draft"),
@@ -9,6 +9,7 @@ STATUS = (
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
@@ -20,5 +21,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-# Create your models here.
+		
+class Login(models.Model):
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=128)
--- BlogKing/blog/views.py
+++ BlogKing/blog/views.py
@@ -1,3 +1,51 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
+from django.contrib.auth import authenticate, login
+from django import forms
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    
+class UserCreationForm(forms.Form):
+    Username = forms.CharField(label='Username', max_length=64)
+    Password = forms.CharField(label='Password', max_length=32)
+
+
+def Register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            Username = form.cleaned_data['Username']
+            Password = form.cleaned_data['Password']
+            user = User.objects.create_user(Username, 'test@gmail.com', Password)
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL
+            return HttpResponseRedirect('/admin/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'register.html', {'form':form})
+
+def my_login_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect('/success/')
+    else:
+        return HTTPResponse('Invalid login')
