e900e9632c681503259400f5b2469daede738885
Averasi <56447978+Averasi@users.noreply.github.com>
2022-12-12 14:49:37 -0600

    Add files via upload

--- BlogKing/blog/models.py
+++ BlogKing/blog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.forms import ModelForm
 
 STATUS = (
     (0,"Draft"),
     (1,"Publish")
 )
- 
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -21,7 +21,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
+		
 class Login(models.Model):
     username = models.CharField(max_length=32)
     password = models.CharField(max_length=128)
--- BlogKing/blog/views.py
+++ BlogKing/blog/views.py
@@ -1,8 +1,11 @@
 from http.client import HTTPResponse
+from django.shortcuts import render
 from django.views import generic
 from .models import Post
 from django.contrib.auth import authenticate, login
+from django import forms
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 
 
 class PostList(generic.ListView):
@@ -12,6 +15,26 @@ class PostList(generic.ListView):
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+    
+class UserCreationForm(forms.Form):
+    Username = forms.CharField(label='Username', max_length=64)
+    Password = forms.CharField(label='Password', max_length=32)
+
+
+def Register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            Username = form.cleaned_data['Username']
+            Password = form.cleaned_data['Password']
+            #unsure if we'll use emails, so prepoulate the argument with a placeholder
+            user = User.objects.create_user(Username, 'placeholder@gmail.com', Password)
+            return HttpResponseRedirect('/login/')
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'register.html', {'form':form})
 
 def my_login_view(request):
     username = request.POST['username']
