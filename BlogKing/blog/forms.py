7c25b8aa25d01beab12ee2efba25aa94841cd6e6
xavihigg <35505036+xavihigg@users.noreply.github.com>
2022-12-14 05:33:37 -0600

    finished

---
+++ BlogKing/blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'slug', 'content', 'status')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }
